8766ff61663b3ab03baff32eac5b692e81d2ad35
Nallelyht <you@example.com>
2017-04-03 23:19:13 -0500

    twitter funcional ninja :P

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
+// variable para el contador de clicks
+var contador = 0;
+//variable para guardar el id de la textarea
+var tweet = document.getElementById("textarea");
+//variable para guardar el id de la entrada llamada autor
+var autor = document.getElementById("autor");
+
+//eventos para el contador de clicks, cuando se da click en la textarea o el input no cuenta el click
+document.addEventListener("click", contadorClick);
+tweet.addEventListener("click", sinClick);
+autor.addEventListener("click", sinClick);
+
+/*funcion para mostrar los tweets, se declara la variable ponerTweet para guardar el id donde se va a enviar los nuevos tweets, las variables nTweet, linea,salto crean nuevos elementos, div, hr y br respectivamente, en el div se guarda el valor de la text area y el autor, todo se agrega al nodo ponerTweet*/
 function mostrarTweet(){
-    var tweet = document.getElementById("textarea");
-    var autor = document.getElementById("autor");
+    
     var ponerTweet = document.getElementById("nuevoTweet");
     var nTweet = document.createElement("div");
     var linea = document.createElement("hr");
+    var salto = document.createElement("br");
     
-    nTweet.innerHTML = tweet.value +" Por "+ autor.value;
+    nTweet.innerHTML = tweet.value +"<br> Por "+ autor.value;
+
     ponerTweet.appendChild(nTweet);
     ponerTweet.appendChild(linea);
     }
     
+/*funcion para contar las letras, se declara la valiable tweet que va a guardar el valor de la text area, se envia al id contador la cantidad de letras que entrar en la textarea, se crea una condicional si los caracteres pasan de 140*/
 function contadorLetras(){
     var tweet = document.getElementById("textarea").value
     
@@ -19,9 +34,18 @@ function contadorLetras(){
     }
 }
 
-function contadorClick (){
-    var contador = 0;
-    var mostrarC = document.getElementById("contadorClick");
+/*funcion contador de clicks se incrementa el contador cadavez que se da click y se manda al contadorClicks */
+function contadorClick(){
     contador++;
-    mostrarC.innerHTML="Contador de clicks: "+ contador;
+    document.getElementById("contadorClicks").innerHTML=contador;
+}
+/*funcion que detiene el evento contador de click */
+function sinClick(){
+    event.stopPropagation();
+}
+/*funcion para limpiar la textarea y el input, tambien reinicia el contador de letras */
+function borrar(){
+    document.getElementById("textarea").value = "";
+    document.getElementById('autor').value="";
+    contadorLetras();
 }
